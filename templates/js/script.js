020c5505ea9c48e352f9dafba0f43aaebd54c8fc
gluv08 <gluv08@yahoo.com>
2017-10-28 14:07:40 +0100

    Add navigation bar on the carousel, shift the signup/sigin and other links further to the right

--- templates/js/script.js
+++ templates/js/script.js
@@ -14,47 +14,17 @@ $(function() {
 		$(this).remove();
 	});
 
-	// $(window).resize(function() {
-	// 	let wheight = $(window).height(); //get the heoght of the window
-	// 	let wheight2 = Math.floor($(window).height() *  0.87); //get the heoght of the window
+	$(window).resize(function() {
+		let wheight = $(window).height(); //get the heoght of the window
+		let wheight2 = Math.floor($(window).height() *  0.87); //get the heoght of the window
 
-	// 	$('.fullheight').css('height', wheight);
-	// 	$('.fullheight2').css('height', wheight2);
+		$('.fullheight').css('height', wheight);
+		$('.fullheight2').css('height', wheight2);
 
-	// });
+	});
 
-  $(window).scroll(function() { // check if scroll event happened
-    if ($(document).scrollTop() > (wheight - 35)) { // check if user scrolled more than 50 from top of the browser window
-      $(".navbar.navigationBar").css("background-color", "rgba(187, 171, 57, 0.9)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-      //$(".navbar-collapse.show").css("background-color", "none");
-      //$(".navbar-collapse").removeClabass("show");
-    } else {
-      $(".navbar.navigationBar").css("background-color", "transparent"); // if not, change it back to transparentss
-     // $(".navbar-collapse").addClass("show");
-    }
-
-    if ($(document).scrollTop() > (wheight2 - 35)) { // check if user scrolled more than 50 from top of the browser window
-      $(".navbar.navigationBar2").css("background-color", "#F63A59"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-    } else {
-      $(".navbar.navigationBar2").css("background-color", "transparent"); // if not, change it back to transparent
-    }
-  });
 
   $(".navbar-nav").clone().prependTo("#off-canvas");
   $(document).trigger("enhance");
-
-
-
- //  let windowsize = $(window).width();
-
-	// $(window).resize(function() {
-	//   windowsize = $(window).width();
-
-	//   if (windowsize <= 768) {
-	// 	// $("nav .container .navbar-header + div").toggleClass("show");
-	//   }else {
-	//   	 //$(".navbar-collapse.show").css("background-color", "");
-	//   }
-	// });
 
 });
