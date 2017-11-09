438b8bdde9990bc6b1ed8e2f6c63131c403a7497
gluv08 <gluv08@yahoo.com>
2017-11-09 10:58:09 +0100

    update node_env

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
 const db = {};
