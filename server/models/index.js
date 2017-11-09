08929a91eda4f550619c3e86862bc20bda66f45f
gluv08 <gluv08@yahoo.com>
2017-11-09 10:47:18 +0100

    fix db connnection issue

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require('../config/config.json')[env];
 
 const db = {};
