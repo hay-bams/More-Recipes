2538cb67682a7eaee016c4af709f85c8b3ef4dd7
purpose50 <purposenigeria@gmail.com>
2018-02-20 02:43:04 +0100

    bug(recipes): bug validate recipe on the client side
    -validate add recipe on the client side
    -validate edit recipe on the client side
    
    [Finishes #155347740]

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxPromise from 'redux-promise';
 import allReducers from '../store/reducers';
