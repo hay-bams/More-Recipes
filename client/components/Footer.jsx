2318b5b26ec636d858cfcf1f96c173536d96bbe8
purpose50 <purposenigeria@gmail.com>
2018-02-27 22:31:29 +0100

    feature(profile picture): add gravatar image for users
    -install react-gravatar
    -display gravatar image
    
    [Finishes #155570415]

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <div className="container">
-      <p className="text-center"><span className="phone">Call us at +2348093747838</span></p>
+      <div className="row">
+        <span className="mx-auto col-lg-3 col-md-4 col-sm-6 col-8 phone">Call us at +2348093747838</span>
+      </div>
     </div>
   </footer>
 );
